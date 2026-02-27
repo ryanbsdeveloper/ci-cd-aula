c4a48825a98ac5c12b42dd5ab589c7f5c3e84a7b
Ryan Barbosa <ryanbsdeveloper@gmail.com>
2026-02-26 22:30:31 -0300

    tests: adicionando testes

---
+++ tests/test_home_title.spec.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+beforeAll(() => {
+  const html = fs.readFileSync(
+    path.resolve(__dirname, '../views/index.html'),
+    'utf8'
+  );
+  document.documentElement.innerHTML = html;
+});
+
+test('title exists', () => {
+  expect(document.title).toBe('CI/CD Pipeline Demo');
+});
